110002abfd25dd84fba115fba6685d7bc8ea55be
Trung Nguyen <dtrung2702@gmail.com>
2026-07-05 13:55:17 +0700

    Update index.js

--- insight-hire/src/router/index.js
+++ insight-hire/src/router/index.js
@@ -1,9 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import JobList from '../components/JobExplorer/JobList.vue'
-
+import JobExplorer from '../components/JobExplorer/JobExplorer.vue'
+import JobDetails from '../components/JobExplorer/JobDetails.vue'
+import JobPlaceholder from '../components/JobExplorer/JobPlaceholder.vue'
+import JobOverview from '../components/JobExplorer/JobOverview.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
-    { path: '/', component: JobList },
+    {
+        path: '/',
+        component: Home,
+        children: [
+            {
+                path: '',
+                name: 'home',
+                component: JobPlaceholder
+            },
+            {
+                path: 'jobs/:jobId',
+                name: 'job-overview',
+                component: JobOverview,
+                props: true
+            },
+            {
+                path: 'jobs/:jobId/details',
+                name: 'job-details',
+                component: JobDetails,
+                props: true
+            }
+        ]
+    }
 ]
 
 const router = createRouter({
