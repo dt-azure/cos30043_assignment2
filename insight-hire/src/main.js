e2398fcfd5174b116323f44477c5787ff54f28f4
Trung Nguyen <dtrung2702@gmail.com>
2026-07-05 13:55:15 +0700

    Update main.js

--- insight-hire/src/main.js
+++ insight-hire/src/main.js
@@ -6,6 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+import { PulseLoader } from 'vue-spinner'
+import 'vue-spinner/style.css'
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+app.component('PulseLoader', PulseLoader)
