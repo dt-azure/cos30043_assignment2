d7c6800570b7cbbdb7423df83a66f3ef444c62c3
Trung Nguyen <dtrung2702@gmail.com>
2026-07-05 13:53:14 +0700

    Update App.vue

--- insight-hire/src/App.vue
+++ insight-hire/src/App.vue
@@ -1,22 +1,12 @@
 <script setup>
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
-import Banner from './components/Banner.vue'
-import JobExplorer from './components/JobExplorer/JobExplorer.vue'
-import ApplicationForm from './components/ApplicationForm.vue'
-import ToDoList from './components/ToDoList.vue'
 </script>
 
 <template>
   <Header />
-
-  <Banner />
   
-  <JobExplorer />
-
-  <ApplicationForm />
-
-  <ToDoList />
+  <RouterView />
 
   <Footer />
 </template>
