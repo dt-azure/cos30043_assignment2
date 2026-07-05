773c5a5507de63fcac5aadf9010960ee0d8ec993
Trung Nguyen <dtrung2702@gmail.com>
2026-07-05 13:53:34 +0700

    Update Footer.vue

--- insight-hire/src/components/Footer.vue
+++ insight-hire/src/components/Footer.vue
@@ -3,5 +3,15 @@
 </script>
 
 <template>
-    <p>Test</p>
+    <footer>
+        <div class="container-header py-3 d-flex align-items-center justify-content-center">
+            <i class="fa-regular fa-copyright me-1"></i>
+            <p class="mb-0">InsightHire</p>
+            <p class="mx-3 mb-0">|</p>
+            <p class="mb-0">Design by Nguyen Duc Trung</p>
+        </div>
+    </footer>
 </template>
+
+<style scoped lang="scss" src="./Footer.scss">
+</style>
